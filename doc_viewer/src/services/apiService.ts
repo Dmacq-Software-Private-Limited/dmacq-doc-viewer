f3b7f21c8b6bfdd91bd4a098567c9b33ef0b4e5e
Atiq Khan <127485586+khan-atiq@users.noreply.github.com>
2025-08-04 12:52:50 +0530

    vite env variable

--- doc_viewer/src/services/apiService.ts
+++ doc_viewer/src/services/apiService.ts
@@ -2,7 +2,9 @@
 
 //const API_BASE_URL = 'http://localhost:8001/api'
 
-const API_BASE_URL = 'http://13.203.247.119:8001/api';
+// const API_BASE_URL = 'http://13.203.247.119:8001/api';
+// const API_BASE_URL = 'http://192.168.0.93:8001/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 
 
