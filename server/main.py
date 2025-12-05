814c0184ce147e320529c67176ff3f8a3a3b822c
dMACQ Softwares Pvt. Ltd. <srikant@dmacq.com>
2025-12-05 11:23:52 +0530

    Update main.py

--- server/main.py
+++ server/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", "http://localhost:3000", "http://localhost:8081",
         "http://localhost:8000", "http://localhost:8080", "http://localhost:8001",
-        "http://13.203.247.119", "http://192.168.0.93"
+        "http://192.168.0.93"
     ],
     allow_credentials=True,
     allow_methods=["*"],
