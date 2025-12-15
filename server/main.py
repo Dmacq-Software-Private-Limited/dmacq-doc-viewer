6d4ea7e9493c1a7385aac11c33c79f79b74a56d7
dMACQ Softwares Pvt. Ltd. <srikant@dmacq.com>
2025-12-15 14:52:30 +0530

    Update main.py

--- server/main.py
+++ server/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
         "http://localhost:5173", "http://localhost:3000", "http://localhost:8081",
         "http://localhost:8000", "http://localhost:8080", "http://localhost:8001",
         "http://192.168.0.93:8081", "http://192.168.0.93:8001", "http://192.168.0.93:8080",
-        "https://docviewer-dev.dmacq.app/"
+        "https://docviewer-dev.dmacq.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
